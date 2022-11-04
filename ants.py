926e667157780c9701cccbad14ce5f3c9e731018
Nate Gonzales Hess <nhess@uoregon.edu>
2022-11-04 01:51:05 -0700

    marching ants problem

--- ants.py
+++ ants.py
@@ -2,90 +2,36 @@ import numpy as np
 import random
 import cv2
 
-stick = 500
+stick = 1001
 height = 100
 width = stick
-
-state1 = [i for i in range(0, stick, 50)]
-state2 = []
 direction = [-1, 1]
+ants = np.array([i for i in range(0, stick, 50)])
+step = [random.choice(direction) for x in range(len(ants))]
 match = 0
 
-for i in state1:
-    walk = random.choice(direction)
-    new = i+walk
-    state2.append(new)
-
-
-
 while True:
-    count = 0
-    #Clear frame
+
+    # Clear frame
     display = np.zeros((height, width, 3), dtype=np.uint8)
 
-    #print(state2)
-    #print(state1)
+    # Animate
+    for x in range(1, len(ants)-1):
+        ants[x] = ants[x]+step[x]
+        if ants[x] == stick:
+            ants[x] = 0
 
-    for i in range(1, len(state1)-1):
-        count += 1
+        # Collision detection and direction toggle
+        match = np.where(ants == ants[x])[0]
+        if len(match) == 2:
+            step[match[0]] *= -1
+            step[match[1]] *= -1
 
-        # Continue in previous direction
-        if state1[i] > state2[i]:
-            toggle = -1
-            new = state2[i] + toggle
-            state1[i] = state2[i]
-            state2[i] = new
-            # Print timestamp
-            cv2.putText(display, f'{i}', (new, int(height/2)-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 255))
-
-        elif state1[i] < state2[i]:
-            toggle = +1
-            new = state2[i] + toggle
-            state1[i] = state2[i]
-            state2[i] = new
-            # Print timestamp
-            cv2.putText(display, f'{i}', (new, int(height/2)-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 255))
-
-        try:
-            match = state1.index(new)
-            print("collision")
-            if state1[i] == state2[i]+1:
-                toggle = 2
-                #new = state2[i] + toggle
-
-                #state2[i] = new
-                state2[i] = state2[i]+toggle
-                state1[i] = state1[i]-toggle
-                state2[match] = state2[match] - toggle
-                print("reverse")
-
-            if state1[i] == state2[match]-1:
-                toggle = -2
-                #new = state2[i] + toggle
-
-                #state2[i] = new
-                state2[i] = state2[i]+toggle
-                state1[i] = state1[i]-toggle
-                state2[match] = state2[match] - toggle
-                print("reverse")
-        except:
-            print("")
-
-
-
-        # Capture dots as they exit
-        if new >= 498:
-            new = 499
-        if new <= 2:
-            new = 1
-
-        # Edit image
-        display[int(height/2), new] = (0, 0, 255)
-
-
+        # Draw ants and labels
+        cv2.putText(display, f'{x+1}', (ants[x], int(height/2)-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=.25, color=(255, 255, 255))
+        display[int(height/2), ants[x]] = (0, 0, 255)
 
     # Display Results
     cv2.imshow('Ants', display)
-    #cv2.imwrite("output/lsr-rt/" + str(i) + ".png", display)
     cv2.waitKey(16)
 
